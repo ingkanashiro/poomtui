c3b3ecd0c4f6ed21d9ca7f2986d8b31e888c87fa
ingkanashiro <santiagogavilanok@gmail.com>
2026-09-11 00:57:40 -0500

    feat: deduction list working

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
-    DefaultTerminal, Frame,
-    crossterm::{
-        event::{self, Event},
-        terminal,
-    },
-    layout::{Constraint, Layout},
-    style::{Color, Stylize},
-    widgets::{Block, BorderType::Thick, List, ListItem, Paragraph, Widget},
+    DefaultTerminal, Frame, crossterm::{
+        event::{self, Event, KeyEvent}, terminal,
+    }, layout::{Constraint, Layout}, style::{Color, Style, Stylize}, widgets::{Block, BorderType::{Double, Thick}, List, ListItem, ListState, Paragraph, Widget},
 };
 
 #[derive(Debug, Default)]
@@ -16,6 +11,11 @@ struct AppState {
     pres_scores: Vec<Score>,
 
     deductions: Vec<Deduction>,
+    deductions_state: ListState,
+
+    is_add_deduction: bool,
+    is_add_deduction_hard: bool,
+    input_value: String,
 }
 
 #[derive(Debug, Default)]
@@ -29,24 +29,30 @@ struct Score {
     value: f32,
 }
 
+enum FormAction {
+    None,
+    Submit,
+    Escape
+}
+
 fn main() -> Result<()> {
     let mut state = AppState::default();
+    state.is_add_deduction = false;
 
     // TEST VALUES FOR DEDUCTIONS LIST
-    state.deductions.push(Deduction{
+    state.deductions.push(Deduction {
         value: 0.3,
-        desc: String::from("missed mandatory dwit kubi")
+        desc: String::from("missed mandatory dwit kubi"),
     });
 
-    state.deductions.push(Deduction{
+    state.deductions.push(Deduction {
         value: 0.3,
-        desc: String::from("missed mandatory boom seogi")
+        desc: String::from("missed mandatory boom seogi"),
     });
-    state.deductions.push(Deduction{
+    state.deductions.push(Deduction {
         value: 0.1,
-        desc: String::from("overtime")
+        desc: String::from("overtime"),
     });
-
 
     color_eyre::install()?;
 
@@ -64,11 +70,38 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
 
         // Input handling
         if let Event::Key(key) = event::read()? {
-            match key.code {
-                event::KeyCode::Esc => {
+            
+            if app_state.is_add_deduction {
+                match handle_add_deduction(key, app_state) {
+                    FormAction::None => {},
+                    FormAction::Submit => {
+                        app_state.is_add_deduction = false;
+
+                        if app_state.is_add_deduction_hard {
+                            app_state.deductions.push(Deduction {
+                                value: 0.1,
+                                desc: app_state.input_value.clone(),
+                            })
+                        }
+                        else {
+                            app_state.deductions.push(Deduction {
+                                value: 0.3,
+                                desc: app_state.input_value.clone(),
+                            })
+                        }
+
+                        app_state.input_value.clear();
+                    },
+                    FormAction::Escape => {
+                        app_state.is_add_deduction = false;
+                        app_state.input_value.clear();
+                    },
+                }
+            } 
+            else {    
+                if handle_key(key, app_state) {
                     break;
                 }
-                _ => {}
             }
         }
     }
@@ -76,7 +109,68 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, app_state: &AppState) {
+fn handle_add_deduction(key: KeyEvent, app_state: &mut AppState) -> FormAction {
+
+    match key.code {
+
+        event::KeyCode::Char(c) => {
+            app_state.input_value.push(c);
+        }
+
+        event::KeyCode::Backspace => {
+            app_state.input_value.pop();
+        }
+
+        event::KeyCode::Enter => {
+            return FormAction::Submit;
+        }
+
+        event::KeyCode::Esc => {
+            return FormAction::Escape;
+        }
+        _ => {}
+    }
+    
+    FormAction::None
+}
+
+fn handle_key(key: KeyEvent, app_state: &mut AppState) -> bool {
+    match key.code {
+        event::KeyCode::Esc => {
+            return true;
+        }
+        event::KeyCode::Char(char) => match char {
+            'ñ' => {
+                app_state.deductions_state.select_previous();
+            }
+            '{' => {
+                app_state.deductions_state.select_next();
+            }
+            '+' => {
+                app_state.deductions_state.select_last();
+                app_state.is_add_deduction = true;
+                app_state.is_add_deduction_hard = true;
+            }
+            '}' => {
+                app_state.deductions_state.select_last();
+                app_state.is_add_deduction = true;
+                app_state.is_add_deduction_hard = false;
+            }
+            '-' => {
+                if let Some(index) = app_state.deductions_state.selected() {
+                    app_state.deductions.remove(index);
+                }
+            }
+
+            _ => {}
+        },
+        _ => {}
+    }
+
+    return false;
+}
+
+fn render(frame: &mut Frame, app_state: &mut AppState) {
     let [border_area] = Layout::vertical([Constraint::Fill(1)])
         .margin(1)
         .areas(frame.area());
@@ -90,11 +184,20 @@ fn render(frame: &mut Frame, app_state: &AppState) {
         .fg(Color::Red)
         .render(border_area, frame.buffer_mut());
 
-    List::new(
+    let deduction_list = List::new(
         app_state
             .deductions
             .iter()
-            .map(|x| ListItem::from(format!("* [{}] {}", x.value.clone(), x.desc.clone()))),
+            .map(|x| ListItem::from(format!(" * [{}] -> {}", x.value.clone(), x.desc.clone()))),
     )
-    .render(inner_area, frame.buffer_mut());
+    .highlight_style(Style::default().fg(Color::Black).bg(Color::Red).bold());
+
+    frame.render_stateful_widget(deduction_list, inner_area, &mut app_state.deductions_state);
+
+    if app_state.is_add_deduction {
+        Paragraph::new(
+            app_state.
+            input_value.as_str()
+        ).render(frame.area(), frame.buffer_mut());
+    }
 }
